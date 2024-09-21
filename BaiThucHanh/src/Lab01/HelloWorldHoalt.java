7128739f759da87e437ab1811f2689e6321c3648
hoa20201 <lethihoa.hust@gmail.com>
2024-09-21 09:05:38 +0700

    HelloWorldHoalt.java
    Cập nhật HellloWorld

--- BaiThucHanh/src/Lab01/HelloWorldHoalt.java
+++ BaiThucHanh/src/Lab01/HelloWorldHoalt.java
@@ -4,7 +4,8 @@ public class HelloWorldHoalt {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+     System.out.print("\nTôi là Lê Thị Hoa, Xin chào bạn!");
+     System.out.print("\nTôi đến từ Nghệ An");
 	}
 
 }
