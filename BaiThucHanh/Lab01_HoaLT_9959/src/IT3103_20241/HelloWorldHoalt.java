c95c3896942e7679edb3b7bfd669c7e240ffaf18
hoa20201 <lethihoa.hust@gmail.com>
2024-09-25 15:53:57 +0700

    BaiThucHanh

---
+++ BaiThucHanh/Lab01_HoaLT_9959/src/IT3103_20241/HelloWorldHoalt.java
@@ -0,0 +1,10 @@
+package IT3103_20241;
+
+public class HelloWorldHoalt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chao ban! Toi la Le Thi Hoa");
+	}
+
+}
---
+++ BaiThucHanh/Lab01_HoaLT_9959/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Lab01_HoaLT_9959 {
+	requires java.desktop;
+}
